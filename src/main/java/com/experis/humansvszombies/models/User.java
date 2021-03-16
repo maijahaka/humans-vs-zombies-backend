e6d2ec6c69a008b21b1f8fc1798615cc3e8740e3
okarp <ky6034@kolumbus.fi>
2021-03-16 11:12:41 +0200

    feat: add user model

---
+++ src/main/java/com/experis/humansvszombies/models/User.java
@@ -0,0 +1,15 @@
+package com.experis.humansvszombies.models;
+
+import javax.persistence.*;
+
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+}
