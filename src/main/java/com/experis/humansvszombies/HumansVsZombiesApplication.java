cc3d72389bacfee14968addf17ef1b55c8549a3b
maija.haka <maija.haka@5CG05206QX.nordic.local>
2021-03-16 10:39:41 +0200

    build: initialise Spring Boot

---
+++ src/main/java/com/experis/humansvszombies/HumansVsZombiesApplication.java
@@ -0,0 +1,13 @@
+package com.experis.humansvszombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumansVsZombiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumansVsZombiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experis/humansvszombies/HumansVsZombiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.humansvszombies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumansVsZombiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
