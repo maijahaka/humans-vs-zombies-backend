dc5e576032201ea2b2a61de14e523a9aa360163d
okarp <ky6034@kolumbus.fi>
2021-03-16 11:16:16 +0200

    chore: add getters and setters

--- src/main/java/com/experis/humansvszombies/models/User.java
+++ src/main/java/com/experis/humansvszombies/models/User.java
@@ -1,10 +1,9 @@
 package com.experis.humansvszombies.models;
-
 import javax.persistence.*;
 
 @Entity
-
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -12,4 +11,28 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
